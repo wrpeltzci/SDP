3e5b95abf7b8cb6ec4321fae3ca407ccc064c9e3
ez <ezcabrera@gmail.com>
2021-12-20 22:59:41 -0600

    Added environment variables properly

--- Firestore.js
+++ Firestore.js
@@ -1,16 +1,24 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import dotenv  from "dotenv";
+
+dotenv.config();
 
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyDgt-zDXY8SOawuKIFVn6o1Mjddzmlqg-k",
-  authDomain: "pdfbuilder-ce81a.firebaseapp.com",
-  projectId: "pdfbuilder-ce81a",
-  storageBucket: "pdfbuilder-ce81a.appspot.com",
-  messagingSenderId: "287077663365",
-  appId: "1:287077663365:web:4fe58349f6a64fa7f4dfec",
-  measurementId: "G-56RHH01JE9",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 });
 
 const db = getFirestore(firebaseApp);
 
 export default db;
+
+
+
+
+
